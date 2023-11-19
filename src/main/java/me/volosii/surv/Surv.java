5ede533e0f218aa230b104ee64bffad3511119a1
Volosii <lechakiss@gmail.com>
2023-11-19 04:36:50 +0300

    whitef

--- src/main/java/me/volosii/surv/Surv.java
+++ src/main/java/me/volosii/surv/Surv.java
@@ -1,15 +1,19 @@
 package me.volosii.surv;
 
+import me.volosii.surv.block.FlowerShootBlock;
 import me.volosii.surv.block.FrozenBushBlock;
 import me.volosii.surv.world.feature.SnowPlantFeature;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.minecraft.block.Block;
 import net.minecraft.class_231;
+import net.minecraft.item.ItemStack;
 import net.modificationstation.stationapi.api.client.event.texture.TextureRegisterEvent;
 import net.modificationstation.stationapi.api.client.texture.atlas.Atlases;
+import net.modificationstation.stationapi.api.event.recipe.RecipeRegisterEvent;
 import net.modificationstation.stationapi.api.event.registry.BlockRegistryEvent;
 import net.modificationstation.stationapi.api.event.world.gen.WorldGenEvent;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
+import net.modificationstation.stationapi.api.recipe.CraftingRegistry;
 import net.modificationstation.stationapi.api.template.block.TemplatePlantBlock;
 import net.modificationstation.stationapi.api.util.Namespace;
 
@@ -19,10 +23,14 @@ public class Surv {
     Block whiteFlower;
     Block purpleFlower;
     Block frozenBush;
+    Block whiteFlowerShoot;
+    Block purpleFlowerShoot;
     @EventListener
     void registerBlock(BlockRegistryEvent event) {
-        whiteFlower = new TemplatePlantBlock(namespace.id("white_flower"),0);
-        purpleFlower = new TemplatePlantBlock(namespace.id("purple_flower"),0);
+        whiteFlower = new TemplatePlantBlock(namespace.id("white_flower"), 0);
+        whiteFlowerShoot = new FlowerShootBlock(namespace.id("white_flower_shoot"), whiteFlower);
+        purpleFlower = new TemplatePlantBlock(namespace.id("purple_flower"), 0);
+        purpleFlowerShoot = new FlowerShootBlock(namespace.id("purple_flower_shoot"), purpleFlower);
         frozenBush = new FrozenBushBlock(namespace.id("frozen_bush"),0);
     }
     @EventListener
@@ -55,6 +63,18 @@ public class Surv {
         purpleFlower.textureId = Atlases.getTerrain().addTexture(namespace.id("block/purpleflower")).index;
         purpleFlower.asItem().method_458(purpleFlower.textureId);
         frozenBush.textureId = Atlases.getTerrain().addTexture(namespace.id("block/frozenbush")).index;
+        purpleFlowerShoot.textureId = whiteFlowerShoot.textureId = Atlases.getTerrain().addTexture(namespace.id("block/flowershoot")).index;
+        int flowerSeedTexture = Atlases.getTerrain().addTexture(namespace.id("block/flowerseed")).index;
+        whiteFlowerShoot.asItem().method_458(flowerSeedTexture);
+        purpleFlowerShoot.asItem().method_458(flowerSeedTexture);
         frozenBush.asItem().method_458(frozenBush.textureId);
+
+    }
+    @EventListener
+    void registerRecipe(RecipeRegisterEvent event) {
+        if(event.recipeId == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPELESS.type()) {
+            CraftingRegistry.addShapelessRecipe(new ItemStack(whiteFlowerShoot),whiteFlower);
+            CraftingRegistry.addShapelessRecipe(new ItemStack(purpleFlowerShoot),purpleFlower);
+        }
     }
 }
